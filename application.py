89dceb2c2fca8be235d39e27a4a8ea815228bf66
Ben Bernardy <benbernardy@Bens-MacBook-Pro.local>
2017-08-07 10:50:42 -0700

    added full file path

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ import pygame
 pygame.init()
 pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize
 
-pygame.mixer.music.load('01ANightOfDizzySpells.ogg')
+pygame.mixer.music.load('/Users/benbernardy/source/audiodome/01ANightOfDizzySpells.ogg')
 pygame.mixer.music.play(0)
 
 pygame.event.wait()
