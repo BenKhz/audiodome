4e38cf9d00dbb77e3774059821d0f82784395a81
Ben Bernardy <benbernardy@Bens-MacBook-Pro.local>
2017-08-07 10:08:29 -0700

    Added source audio mp3 and changed to ref mp3

--- application.py
+++ application.py
@@ -1,12 +1,13 @@
 import time
 
 # Start pygame music and play song in audiodome file.
+
 import pygame
 
 pygame.init()
 pygame.mixer.init()
 
-pygame.mixer.music.load('01_A_Night_Of_Dizzy_Spells.ogg')
+pygame.mixer.music.load('01_A_Night_Of_Dizzy_Spells.mp3')
 pygame.mixer.music.play(0)
 
 pygame.event.wait()
