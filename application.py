4c37950f988d252e7d8f3fa17185328e16eebfc6
Ben Bernardy <benbernardy@Bens-MacBook-Pro.local>
2017-08-07 19:59:39 -0700

    Using WAV audio format this time around

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ import pygame
 pygame.init()
 pygame.mixer.pre_init(44100, 16, 2, 4096) #frequency, size, channels, buffersize
 
-pygame.mixer.music.load('01ANightOfDizzySpells.ogg')
+pygame.mixer.music.load('01ANightOfDizzySpells.wav')
 pygame.mixer.music.play(0)
 
 pygame.event.wait()
