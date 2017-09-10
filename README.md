77b1c0f0cd6868f6ec74698bcf69e32f0e68ff55
ashmastaflash <ash.d.wilson@gmail.com>
2017-09-09 17:37:31 -0700

    chg: dev: Refactor, break notes out of README.md into NOTES.txt, Add comments to code.

--- README.md
+++ README.md
@@ -1,20 +1,45 @@
-# audiodome
+# AudioDome
 
-Set the `AUDIO_SRC` environment variable to
-`http://ice2.somafm.com/defcon-64-aac` or another similarly-encoded stream.
 
-It may work with a URL to a playlist file.
+## Requirements
 
-Simple Notes:
-    Download vs Stream?
-    Download:
-    using urllib to access URL and download.
-        urllib.request.urlretrieve("URL", "Saved FilePath")
-    Possible to use pip install pydrive and use that to download?
+* Accounts:
+  * Resin.io
+* Hardware:
+  * Raspberry Pi 2 or 3
+  * Speakers for the Resin-managed Pis
 
-Stream:
-   Use headless VLC player to connect to an icecast? Do I need control?
+## Usage
 
-pip install dbus-python #dependency for vlc.ctrl
+1. Create a Resin application for AudioDome
+1. Clone down this repository and add the Resin.io app as a git remote.
+1. Configure your environment variables according to your use case (see table
+  below and the Implementation Notes section).
+1. Push this repository to your Resin.io app
+1. Image one or more Raspberry Pis for your Resin.io app
 
-pip install vlc.ctrl #to control VLC through Dbus
+| Environment Variable     | Purpose                                          |
+|--------------------------|--------------------------------------------------|
+| AUDIO_SRC                | Path or URL for audio file to be played          |
+| GOOGLE_DRIVE_CREDENTIALS | Base64-encoded Google Drive OAuth credentials    |
+| GOOGLE_FILE_ID           | ID of file to be downloaded from Google Drive    |
+
+
+### Implementation Notes
+
+The `GOOGLE_FILE_ID` and `GOOGLE_DRIVE_CREDENTIALS` environment variables are
+optional!  If they're not set, you should set `AUDIO_SRC` to a URL.  
+`http://ice2.somafm.com/defcon-64-aac` works well for this.  If you do not set
+the `AUDIO_SRC` environment variable, the device will play an audio file that's
+embedded in the image itself.
+
+If you want to use Google Drive to deliver media for your AudioDome devices,
+you should follow one of these use cases:
+
+1. The Google Drive file is a zip file containing one audio file, which is
+ referenced (at its expanded location) by the `AUDIO_SRC` environment variable.
+1. The Google Drive file is a zip file containing a number of audio files and
+  a playlist file.  The playlist file is referenced with the `AUDIO_SRC`
+  environment variable.
+1. The Google Drive file is a zip file containing a playlist file, which
+  references URLs that are supported by VLC.
--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 import base64
 import os
+import sys
 import time
 import vlc
 import httplib2
@@ -13,9 +14,10 @@ def main():
     # This is a string that represents the audio to be played.  If the
     # AUDIO_SRC environment variable is not set, it defaults to the song that
     # comes embedded in the docker image.
-
     audio_to_play = os.getenv('AUDIO_SRC', './01ANightOfDizzySpells.ogg')
+    # This is the file we will write the Google API credentials into.
     gdrive_credential_file = "/tmp/resin/gdrive.json"
+    # This is where the music files get written to.
     music_path = "/data/"
 
     if os.getenv('GOOGLE_DRIVE_CREDENTIALS') is not None:
@@ -23,7 +25,7 @@ def main():
         storage = Storage(gdrive_credential_file)
         google_creds = storage.get()
 
-    if os.getenv('GOOGLE_DRIVE_ID') is not None:
+    if os.getenv('GOOGLE_FILE_ID') is not None:
         download_from_google_drive(music_path, google_creds)
 
     player = vlc.MediaPlayer(audio_to_play)
@@ -39,28 +41,42 @@ def main():
 
 
 def download_from_google_drive(music_path, google_creds):
+    '''Download and expand file from Google Drive.
+
+    Args:
+        music_path(str): This is the base path for working with the music
+            download.
+        google_creds(oauth2client.file.Storage): This is an object containing
+            The credentials to be used for Google Drive.
+    '''
     http = httplib2.Http()
     http = google_creds.authorize(http)
+    # The prior line isn't great form, as it re-declares the smae variable as
+    # something of a different type.  We'll leave it for now, and refactor
+    # later.
     drive_service = discovery.build('drive', 'v2', http=http)
     file_id = os.getenv('GOOGLE_FILE_ID')
     drop_file_location = os.path.join(music_path, "google_drop_file.zip")
     with open(drop_file_location, 'w') as drop_file:
-        request = drive_service.files().get_media(fileId=file_id)
+        drive_files = drive_service.files()
+        request = drive_files.get_media(fileId=file_id)
         media_request = http.MediaIoBaseDownload(drop_file, request)
         while True:
             try:
                 download_progress, done = media_request.next_chunk()
             except errors.HttpError, error:
-                print 'An error occurred: %s' % error
-                return
+                print('An error occurred: %s' % error)
+                sys.exit(2)  # If this happens, we exit in a ball of fire.
             if download_progress:
-                print 'Download Progress: %d%%' % int(download_progress.progress() * 100)  # NOQA
+                prg = int(download_progress.progress() * 100)
+                print('Download Progress: %d%%' % prg)
             if done:
-                print 'Download Complete'
-                return
+                print('Download Complete')
+                break
 
 
 def place_drive_credentials(gdrive_credential_file):
+    '''Take Google API keys from env var, write them to file.'''
     b64_creds = os.getenv('GOOGLE_DRIVE_CREDENTIALS')
     with open(gdrive_credential_file, 'w') as cred_file:
         raw_creds = base64.b64decode(b64_creds)
