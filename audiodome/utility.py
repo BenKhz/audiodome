61bc33c913d1ab2518dbf57b3ec69b7c160c6b84
Ben Bernardy <benbernardy@gmail.com>
2017-09-24 21:57:46 -0700

    Added vlc_play_file Utility Class

--- audiodome/utility.py
+++ audiodome/utility.py
@@ -1,5 +1,7 @@
 import base64
 import zipfile
+import vlc
+import time
 
 
 class Utility(object):
@@ -15,3 +17,16 @@ class Utility(object):
             zip_ref.extractall(out_dir)
             print ("Zip extracted!")
         return
+
+    @classmethod
+    def vlc_play_file(cls, audio_to_play):
+        player = vlc.MediaPlayer(audio_to_play)
+        print("Playing %s" % audio_to_play)
+        player.play()
+        time.sleep(2)
+
+        while player.is_playing():
+            print("%s is still playing..." % audio_to_play)
+            time.sleep(60)
+
+        print("%s is done playing!" % audio_to_play)
