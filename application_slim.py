59a08bb04bb83257de7a8e42b1a099c3f087f2f8
Ben Bernardy <benbernardy@gmail.com>
2017-09-26 12:41:33 -0700

    Still messing with playlist pathing.

--- application_slim.py
+++ application_slim.py
@@ -14,9 +14,9 @@ def main():
         if os.path.exists(drop_file_location):
             audiodome.Utility.unzip_file_to_path(drop_file_location,
                                                  music_path)
+            print("30 seconds before attempting to build playlist.")
             time.sleep(30)
-            print("30 seconds to bash")
-            remote_audio_file = open(os.join(music_path, "playlist.txt"))
+            remote_audio_file = open("data/playlist.txt")
             remote_audio = remote_audio_file.read_lines()
             audiodome.Utility.vlc_play_file(remote_audio)
     else:
