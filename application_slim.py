96e912756ea07c086b8fcc5f5ffed485594b1825
Ben Bernardy <benbernardy@gmail.com>
2017-09-26 13:14:51 -0700

    Reverted

--- application_slim.py
+++ application_slim.py
@@ -1,11 +1,10 @@
 import audiodome
 import os
-import time
 
 
 def main():
 
-    local_audio = './01ANightOfDizzySpells.ogg'
+    audio_to_play = os.getenv('AUDIO_SRC', './01ANightOfDizzySpells.ogg')
     music_path = "/data/"
     if os.getenv('DOWNLOAD_URL') is not None:
         drop_file_location = '/data/drop_file.zip'
@@ -14,14 +13,10 @@ def main():
         if os.path.exists(drop_file_location):
             audiodome.Utility.unzip_file_to_path(drop_file_location,
                                                  music_path)
-            print("30 seconds before attempting to build playlist.")
-            time.sleep(30)
-            remote_audio_file = open("../data/playlist.txt")
-            remote_audio = remote_audio_file.read_lines()
-            audiodome.Utility.vlc_play_file(remote_audio)
+
     else:
         print("No download URL. Playing local file...")
-        audiodome.Utility.vlc_play_file(local_audio)
+    audiodome.Utility.vlc_play_file(audio_to_play)
 
 
 if __name__ == "__main__":
