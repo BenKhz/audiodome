a7c21438a7f3a91477c972f7a8ea1fbffb3b10c0
Ben Bernardy <benbernardy@gmail.com>
2017-09-26 22:05:37 -0700

    Added os.listdir() to put music directory into listed variable.

--- application_slim.py
+++ application_slim.py
@@ -13,7 +13,7 @@ def main():
         if os.path.exists(drop_file_location):
             audiodome.Utility.unzip_file_to_path(drop_file_location,
                                                  music_path)
-
+            audio_to_play = os.listdir("../data/music/")
     else:
         print("No download URL. Playing local file...")
     audiodome.Utility.vlc_play_file(audio_to_play)
