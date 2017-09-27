0e091c54af29bb5d7bd91a27062ca55d65efebe3
Ben Bernardy <benbernardy@gmail.com>
2017-09-26 22:14:08 -0700

    Added print after os.listdir to see what that list is....

--- application_slim.py
+++ application_slim.py
@@ -14,6 +14,7 @@ def main():
             audiodome.Utility.unzip_file_to_path(drop_file_location,
                                                  music_path)
             audio_to_play = os.listdir("../data/music/")
+            print(audio_to_play)
     else:
         print("No download URL. Playing local file...")
     audiodome.Utility.vlc_play_file(audio_to_play)
